1a27f1a4088087c78dbae19b512f1e514bfc51e2
cat3399 <127954618+cat3399@users.noreply.github.com>
2024-03-09 19:46:39 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cp code/open_serial.service /etc/systemd/system/open_serial.service
-cp open_serial.sh /home/open_serial.sh
-chmod +x open_serial.sh
+cp code/open_serial.sh /home/open_serial.sh
+chmod +x /home/open_serial.sh
 sudo systemctl enable open_serial
